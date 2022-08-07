949de00b83acb226586c93e0551fc781c215c544
Ashish <ashish07@gmail.com>
2022-08-07 22:44:53 +0530

    my first commit reactapplication

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import web from "./image/img1.svg";
+import "./App.css";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main-div">
+        <div className="sign-up-container">
+          <div className="sign-up-area">
+            <div className="form">
+              <form action="">
+                <h1>sign up</h1>
+                <input type="text" placeholder="Your name" />
+                <input type="email" placeholder="Your Email" />
+                <input type="number" placeholder="mobile number" />
+                <input type="text" placeholder="Your profession" />
+                <input type="password" placeholder="password" />
+                <input type="password" placeholder="confirm password" />
+                <button>Register</button>
+              </form>
+            </div>
+            <div className="image">
+              <img src={web} alt=".." className="animated" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
